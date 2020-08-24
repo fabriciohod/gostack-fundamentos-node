c67692dc001e8bd0e6e972b63ab5a8c14e94b218
Fabricio Duarte <fabriciohod@hotmail.com>
2020-08-24 20:29:52 -0300

    _

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import app from './app';
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
+  // eslint-disable-next-line no-console
+  console.log('(☆▽☆) Server started on port 3333!');
 });
